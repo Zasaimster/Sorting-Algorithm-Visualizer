47299b3cd4d6e8ec6dcc1a2bd169150ecdb4932e
Zasaimster <Zasaimster@gmail.com>
2021-07-19 22:21:23 -0700

    adjusting the 'sorting speed' will change the speed even if the algorithm is already playing

--- src/components/ArrayVisualizer/ArrayVisualizer.js
+++ src/components/ArrayVisualizer/ArrayVisualizer.js
@@ -38,43 +38,55 @@ class ArrayVisualizer extends React.Component {
 	};
 
 	playAlgorithm = () => {
-		if (this.state.isSorting) {
-			const {visualizedSteps} = this.state;
-			// https://stackoverflow.com/a/37728255 although, a timeout wouldn't work either so I used an interval instead
+		console.log('in');
+		const {isSorting, sortingSpeed} = this.state;
+		if (isSorting) {
+			//https://stackoverflow.com/a/37728255 for timeouts
 
-			var interval = setInterval(() => {
+			// var interval = setInterval(() => {
+			// 	const {isSorting, vsIndex, visualizedSteps} = this.state;
+			// 	this.visualizeNextIteration();
+			// 	if (vsIndex === visualizedSteps.length || !isSorting) clearInterval(interval);
+			// }, sortingSpeed);
+
+			//this solution lets you adjust the sorting speed mid algorithm
+			setTimeout(() => {
 				this.visualizeNextIteration();
-				if (this.state.vsIndex === visualizedSteps.length || !this.state.isSorting) clearInterval(interval);
-			}, this.state.sortingSpeed);
+				const {isSorting, vsIndex, visualizedSteps} = this.state;
+				if (vsIndex !== visualizedSteps.length && isSorting) this.playAlgorithm();
+			}, sortingSpeed);
 		}
 	};
 
 	visualizeNextIteration = () => {
-		let visual = this.state.ref.current;
-		const vsIndex = this.state.vsIndex;
-		const steps = this.state.visualizedSteps;
+		const {vsIndex, visualizedSteps, ref} = this.state;
+		if (vsIndex < visualizedSteps.length) {
+			let visual = ref.current;
 
-		visual.updateColors(vsIndex, steps);
+			visual.updateColors(vsIndex, visualizedSteps);
 
-		this.setState({vsIndex: vsIndex + 1}, () => {
-			if (this.state.vsIndex === steps.length) {
-				visual.handleLastStep();
-				this.setState({
-					isSorting: false,
-					isSorted: true,
-				});
-			}
-		});
+			this.setState({vsIndex: vsIndex + 1}, () => {
+				if (this.state.vsIndex === visualizedSteps.length) {
+					visual.handleLastStep();
+					this.setState({
+						isSorting: false,
+						isSorted: true,
+					});
+				}
+			});
+		}
 	};
 
 	initializeArrays = () => {
+		const {currentAlgorithm, arrSize} = this.state;
+
 		let array = [];
-		let algo = this.state.currentAlgorithm;
+		let algo = currentAlgorithm;
 		let minVal = algo === 'countingSort' ? 1 : 10;
 		minVal = algo === 'radixSort' ? 20 : minVal;
 		let maxVal = algo === 'countingSort' ? 9 : 500;
 
-		for (let i = 0; i < this.state.arrSize; i++) {
+		for (let i = 0; i < arrSize; i++) {
 			array[i] = this.getRandomValue(minVal, maxVal);
 		}
 
@@ -84,28 +96,30 @@ class ArrayVisualizer extends React.Component {
 	};
 
 	changeArraySize = () => {
-		let array = [...this.state.array];
-		let size = parseInt(this.state.arrSize);
+		const {array, currentAlgorithm, arrSize} = this.state;
 
-		let algo = this.state.currentAlgorithm;
+		let arr = [...array];
+		let size = parseInt(arrSize);
+
+		let algo = currentAlgorithm;
 		let minVal = algo === 'countingSort' ? 1 : 10;
 		minVal = algo === 'radixSort' ? 20 : minVal;
 		let maxVal = algo === 'countingSort' ? 9 : 500;
 
-		if (array.length < size) {
+		if (arr.length < size) {
 			//new array is bigger
-			for (let i = array.length; i < size; i++) {
-				array[i] = this.getRandomValue(minVal, maxVal);
+			for (let i = arr.length; i < size; i++) {
+				arr[i] = this.getRandomValue(minVal, maxVal);
 			}
 		} else {
 			//new array is smaller
-			let diff = array.length - size;
+			let diff = arr.length - size;
 			for (let i = 0; i < diff; i++) {
-				array.pop();
+				arr.pop();
 			}
 		}
 
-		this.setState({array}, () => {
+		this.setState({array: arr}, () => {
 			this.getVisualizedSteps();
 		});
 	};
@@ -125,9 +139,11 @@ class ArrayVisualizer extends React.Component {
 	};
 
 	getVisualizedSteps = () => {
+		const {array, currentAlgorithm} = this.state;
+
 		let visualizedSteps = [];
-		let tempArray = [...this.state.array];
-		switch (this.state.currentAlgorithm) {
+		let tempArray = [...array];
+		switch (currentAlgorithm) {
 			case 'bubbleSort':
 				visualizedSteps = bubbleSort(tempArray);
 				break;
@@ -153,27 +169,28 @@ class ArrayVisualizer extends React.Component {
 				console.log('this algorithm has not been implemented yet');
 				return;
 		}
+		console.log(visualizedSteps);
 		this.setState({visualizedSteps});
 	};
 
-	//
 	getRandomValue(min, max) {
 		return Math.floor(Math.random() * (max - min + 1)) + min; //The maximum is inclusive and the minimum is inclusive
 	}
 
 	render() {
-		const {array, currentAlgorithm, isSorting} = this.state;
+		const {currentAlgorithm, array, visualizedSteps, vsIndex, isSorting, arrSize, sortingSpeed, ref, isSorted} = this.state;
 		return (
 			<>
 				<Nav
 					visualizeNextIteration={this.visualizeNextIteration}
-					setPlayAlgorithm={() => this.setState({isSorting: !this.state.isSorting}, () => this.playAlgorithm())}
+					setPlayAlgorithm={() => this.setState({isSorting: !isSorting}, () => this.playAlgorithm())}
 					reset={() => {
 						this.reset();
 					}}
 					chooseAlgorithm={(e) => {
-						this.setState({currentAlgorithm: e.target.value}, () => this.reset());
-						if (e.target.value === 'radixSort') this.setState({arrSize: 15});
+						const {value} = e.target;
+						this.setState({currentAlgorithm: value}, () => this.reset());
+						if (value === 'radixSort' || value === 'countingSort') this.setState({arrSize: 15});
 					}}
 					handleSize={(e) => {
 						this.setState(
@@ -183,7 +200,7 @@ class ArrayVisualizer extends React.Component {
 							},
 							() => {
 								this.changeArraySize();
-								this.state.ref.current.reset();
+								ref.current.reset();
 							}
 						);
 					}}
@@ -192,19 +209,19 @@ class ArrayVisualizer extends React.Component {
 							sortingSpeed: ITERATION_SPEEDS[e.target.value - 1],
 						});
 					}}
-					maxSize={this.state.currentAlgorithm === 'radixSort' ? 20 : MAX_ARR_SIZE}
-					isSorted={this.state.isSorted}
-					arrSize={this.state.arrSize}
+					maxSize={currentAlgorithm === 'radixSort' || currentAlgorithm === 'countingSort' ? 20 : MAX_ARR_SIZE}
+					isSorted={isSorted}
+					arrSize={arrSize}
 				/>
-				{this.state.visualizedSteps.length === 0 && <div> Loading... </div>}
+				{visualizedSteps.length === 0 && <div> Loading... </div>}
 
 				{(currentAlgorithm === 'bubbleSort' || currentAlgorithm === 'selectionSort' || currentAlgorithm === 'insertionSort') && (
 					<BasicVisualization
 						array={array}
 						isSorting={isSorting}
 						updateArray={(array) => this.setState({array})}
-						steps={this.state.visualizedSteps}
-						ref={this.state.ref}
+						steps={visualizedSteps}
+						ref={ref}
 					/>
 				)}
 				{currentAlgorithm === 'mergeSort' && (
@@ -212,8 +229,8 @@ class ArrayVisualizer extends React.Component {
 						array={array}
 						isSorting={isSorting}
 						updateArray={(array) => this.setState({array})}
-						steps={this.state.visualizedSteps}
-						ref={this.state.ref}
+						steps={visualizedSteps}
+						ref={ref}
 					/>
 				)}
 				{currentAlgorithm === 'quickSort' && (
@@ -221,8 +238,8 @@ class ArrayVisualizer extends React.Component {
 						array={array}
 						isSorting={isSorting}
 						updateArray={(array) => this.setState({array})}
-						steps={this.state.visualizedSteps}
-						ref={this.state.ref}
+						steps={visualizedSteps}
+						ref={ref}
 					/>
 				)}
 				{currentAlgorithm === 'countingSort' && (
@@ -230,8 +247,8 @@ class ArrayVisualizer extends React.Component {
 						array={array}
 						isSorting={isSorting}
 						updateArray={(array) => this.setState({array})}
-						steps={this.state.visualizedSteps}
-						ref={this.state.ref}
+						steps={visualizedSteps}
+						ref={ref}
 					/>
 				)}
 				{currentAlgorithm === 'radixSort' && (
@@ -239,8 +256,8 @@ class ArrayVisualizer extends React.Component {
 						array={array}
 						isSorting={isSorting}
 						updateArray={(array) => this.setState({array})}
-						steps={this.state.visualizedSteps}
-						ref={this.state.ref}
+						steps={visualizedSteps}
+						ref={ref}
 					/>
 				)}
 			</>

730e645e15d0f81eb68ab36cae1beb1194b60cbc
Zasaimster <Zasaimster@gmail.com>
2021-07-23 17:26:05 -0700

    added a tooltip to the wiki link icon

--- src/components/InfoLink/InfoLink.js
+++ src/components/InfoLink/InfoLink.js
@@ -15,22 +15,53 @@ const links = {
 export const InfoLink = ({algorithm}) => {
 	//replace with a question mark image
 	return (
-		<Wrapper>
-			<Link href={links[algorithm]} target='_blank' rel='noreferrer'>
-				<img src={question} alt='?' />
-			</Link>
-		</Wrapper>
+		<>
+			<Wrapper>
+				<a href={links[algorithm]} target='_blank' rel='noreferrer'>
+					<img src={question} alt='?' />
+				</a>
+				<ToolTip>Learn more</ToolTip>
+			</Wrapper>
+		</>
 	);
 };
 
-const Wrapper = styled.div`
-	position: relative;
-	width: 32px;
-	height: 32px;
-	margin-top: 10px;
+const ToolTip = styled.span`
+	visibility: hidden;
+	background-color: white;
+	color: black;
+	padding: 5px 3px 5px 3px;
+	border-radius: 5px;
+	font-family: 'Raleway';
+	font-size: 0.75rem;
+
+	position: absolute;
+	z-index: 1;
+	top: 8px;
+	margin-left: 10px;
+
+	opacity: 0;
+	transition: opacity 30ms;
+
+	&::after {
+		content: '';
+		position: absolute;
+		left: 0;
+		top: 25%;
+		margin-left: -5px;
+
+		border-top: 5px solid transparent;
+		border-bottom: 5px solid transparent;
+		border-right: 5px solid rgba(255, 255, 255, 1);
+	}
 `;
 
-const Link = styled.a`
-	position: absolute;
-	bottom: 0;
+const Wrapper = styled.div`
+	position: relative;
+	margin-top: 5px;
+
+	&:hover ${ToolTip} {
+		visibility: visible;
+		opacity: 0.8;
+	}
 `;
